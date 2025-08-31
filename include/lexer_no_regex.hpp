52c424a66e1a519ff25c3b457c51b70abe220970
Muhammad Saad <140126961+MSJUJ@users.noreply.github.com>
2025-08-31 23:00:32 +0500

    Update lexer_no_regex.hpp
    File Reading & Setup

--- include/lexer_no_regex.hpp
+++ include/lexer_no_regex.hpp
@@ -1,1 +1,21 @@
+#pragma once
+#include "token.hpp"
+#include <vector>
+#include <fstream>
+#include <sstream>
 
+using namespace std;
+
+class LexerNoRegex {
+    string source;
+    size_t pos = 0;
+
+public:
+    
+    //File Reading & Setup
+    LexerNoRegex(const string& filename) {
+        ifstream file(filename);
+        stringstream buffer;
+        buffer << file.rdbuf();
+        source = buffer.str();
+    }
