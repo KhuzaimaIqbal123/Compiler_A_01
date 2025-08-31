b4b502a1c1f37ee94d09928f21d0fa37b8e685c3
Muhammad Saad <140126961+MSJUJ@users.noreply.github.com>
2025-08-31 23:02:24 +0500

    Update lexer_no_regex.hpp
    Tokenization

--- include/lexer_no_regex.hpp
+++ include/lexer_no_regex.hpp
@@ -19,3 +19,32 @@ public:
         buffer << file.rdbuf();
         source = buffer.str();
     }
+ //Tokenization 
+    
+    vector<Token> tokenize() {
+        vector<Token> tokens;
+
+        while (pos < source.size()) {
+            char c = source[pos];
+
+            // Skip whitespace 
+            if (c == ' ' || c == '\t' || c == '\n' || c == '\r') { pos++; continue; }
+
+            //  Comments 
+            if (c == '/' && pos + 1 < source.size()) {
+                if (source[pos + 1] == '/') {
+                    string comment;
+                    while (pos < source.size() && source[pos] != '\n') comment += source[pos++];
+                    tokens.push_back(Token(TokenType::T_COMMENT, comment));
+                    continue;
+                } else if (source[pos + 1] == '*') {
+                    string comment = "/*";
+                    pos += 2;
+                    while (pos + 1 < source.size() && !(source[pos] == '*' && source[pos+1] == '/')) comment += source[pos++];
+                    if (pos + 1 < source.size()) { comment += "*/"; pos += 2; }
+                    tokens.push_back(Token(TokenType::T_COMMENT, comment));
+                    continue;
+                }
+            }
+
+    
